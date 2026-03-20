3e2ebdafd1474a706a77fbbc9149e5c794e2bc4d
seres <seres@seres.eu.org>
2026-03-21 00:31:35 +0100

    changed updater to direct

--- src/main.cpp
+++ src/main.cpp
@@ -47,8 +47,9 @@ extern "C" __declspec(dllexport) AddonDefinition *GetAddonDef()
     addon_def.Load = addon_load;
     addon_def.Unload = addon_unload;
     addon_def.Flags = EAddonFlags_None;
-    addon_def.Provider = EUpdateProvider_GitHub;
-    addon_def.UpdateLink = "https://github.com/Seres67/nexus_chat_shorts";
+    addon_def.Provider = EUpdateProvider_Direct;
+    addon_def.UpdateLink =
+        "https://git.seres.eu.org/seres/nexus_chat_shorts/releases/download/latest/libnexus_chat_shorts.dll";
 
     return &addon_def;
 }
